7a789a9f3d56cb9868a93b601842e53a8142f6b0
pvvquang <pvquangbg@gmail.com>
2023-08-06 22:40:44 +0700

    refactor: change schema

--- prisma/migrations/20230806145659_init_data/migration.sql
+++ prisma/migrations/20230806145659_init_data/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "User" (
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "userName" VARCHAR(255) NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -11,15 +12,16 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "AccesToken" (
+CREATE TABLE "AccessToken" (
     "id" SERIAL NOT NULL,
     "userId" VARCHAR(255) NOT NULL,
     "token" VARCHAR(255) NOT NULL,
     "expires" DATE NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "AccesToken_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "AccessToken_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -29,6 +31,7 @@ CREATE TABLE "RefreshToken" (
     "token" VARCHAR(255) NOT NULL,
     "expires" DATE NOT NULL,
     "replacedByToken" VARCHAR(255) NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -39,7 +42,7 @@ CREATE TABLE "RefreshToken" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
-ALTER TABLE "AccesToken" ADD CONSTRAINT "AccesToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "AccessToken" ADD CONSTRAINT "AccessToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
